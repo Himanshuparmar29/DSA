edf090eb5b05673b1d1c9ba7664050e579c71760
himanshuparmar29 <himu9333@gmail.com>
2024-05-14 22:25:07 +0530

    update

---
+++ Stack/infix_to_postfix.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<stack>
+
+using namespace std;
+
+int main()
+{
+    string infix;
+    cout<<"Enter infix expression : ";
+    cin>>infix;
+    char postfix[infix.length()+1];
+    int c=0;
+    for(int i=0;i<infix.length();i++)
+    {
+        
+    }
+}
