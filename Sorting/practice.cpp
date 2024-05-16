a87b32a39f6de90b38469c0c3b967cd53cf7cdd4
himanshuparmar29 <himu9333@gmail.com>
2024-05-16 12:45:32 +0530

    update

--- Sorting/practice.cpp
+++ Sorting/practice.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include<vector>
 using namespace std;
 
 void display(int a[], int n)
@@ -10,16 +10,53 @@ void display(int a[], int n)
     }
     cout << endl;
 }
-void shall(int a,int n)
+int maxnum(int a[],int n)
 {
-    for(int span=n/2;span>=1;span=span/2){
-        
+    int max=a[0];
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    return max;
+}
+void countingsort(int a[],int n,int ex)
+{
+    vector<int> output(n,0);
+    vector<int> count(10,0);
+
+    for(int i=0;i<n;i++)
+    {
+        count[(a[i]/ex)%10]++;
+    }
+
+    for(int i=1;i<10;i++)
+    {
+        count[i]+=count[i-1];
+    }
+    for(int i=n-1;i>=0;i--)
+    {
+        output[--count[(a[i]/ex)%10]]=a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        a[i]=output[i];
+    }
+}
+void radix(int a[],int n)
+{
+    int max=maxnum(a,n);
+    for(int ex=1;max/ex>0;ex*=10)
+    {
+        countingsort(a,n,ex);
     }
 }
 int main()
 {
     int a[] = {201, 71, 44, 37, 29, 18, 4};
     int n = sizeof(a) / sizeof(a[0]);
-    divide(a,0,n-1);
+    radix(a,n);
     display(a,n);
 }
