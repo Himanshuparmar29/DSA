4ab90c35cee505213cc263a51fd37c4eda33d52a
himanshuparmar29 <himu9333@gmail.com>
2024-05-16 12:53:55 +0530

    update

--- Link List/practice.cpp
+++ Link List/practice.cpp
@@ -2,29 +2,27 @@
 
 using namespace std;
 
-void display(int a[],int n)
+class node
 {
-    for(int i=0;i<n;i++)
+    int info;
+    node *next;
+    node *pre;
+    public:
+    node(){}
+    node(int x):info(x)
     {
-        cout<<a[i]<<" ";
+        next=NULL;
+        pre=NULL;
     }
-    cout<<endl;
-}
 
-int main()
-{
-    int arr[]={201,71,44,37,29,18,4};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    for(int i=0;i<n;i++)
+    node * append(int x)
     {
-        for(int j=0;j<n-1-i;j++)
+        node *t=new node(x);
+        node *temp=this;
+        if(temp->info >= x)
         {
-            if(arr[j]>=arr[j+1])
-            {
-                int t=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=t;
-            }
+            temp->next=t;
+            t->next=
         }
     }
-}
+};
